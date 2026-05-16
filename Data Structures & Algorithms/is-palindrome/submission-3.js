636825f2c97ea368e94c2e0328d1183df05e3979
Nathies <149600915+nathies-codes@users.noreply.github.com>
2026-05-16 22:26:37 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let l = 0
+        let r = s.length - 1;
+
+        while ( l < r ) {
+            while ( l < r && !this.alphaNums(s[l])) {
+                l ++;
+            }
+        
+            while ( l < r && !this.alphaNums(s[r])) {
+                r --;
+            }
+
+            if( s[l].toLowerCase() !== s[r].toLowerCase()) 
+            {
+                return false
+            }
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+    alphaNums(a) {
+        return(
+            (a >= 'a' && a <= 'z') ||
+            (a >= 'A' && a <= 'Z') ||
+            (a >= '0' && a <= '9')
+        )
+    }
+}
