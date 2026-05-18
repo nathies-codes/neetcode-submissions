68d54ff71fb9637c12e62d46a6502cd0b386eaca
Nathies <149600915+nathies-codes@users.noreply.github.com>
2026-05-18 22:20:26 +0530

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let result =  {};
+
+        for (let str of strs) {
+
+            const strSorted = str.split('').sort().join();
+
+            if(!result[strSorted]) {
+                result[strSorted] = []
+            }
+
+            result[strSorted].push(str);
+        }
+
+        return Object.values(result);
+    }
+}
