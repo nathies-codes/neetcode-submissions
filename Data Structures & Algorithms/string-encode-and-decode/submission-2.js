4b4c83d67452e86d0ade94b7fddb1c6392fc8f14
Nathies <149600915+nathies-codes@users.noreply.github.com>
2026-05-27 20:26:43 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let res = ''
+
+        for( let str of strs ) {
+            res += String(str.length) + '#' + str;
+        } 
+
+        return res;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let res = [], i = 0;
+
+        while (i < str.length) {
+            
+            let j = i;
+
+            while( str[j] !== '#' ) {
+                j ++;
+            }
+
+            let length = parseInt(str.substring(i, j));
+
+            i = j + 1;
+            j = i + length;
+
+            res.push(str.substring(i, j));
+
+            i = j;
+        }
+        return res;
+    }
+}
