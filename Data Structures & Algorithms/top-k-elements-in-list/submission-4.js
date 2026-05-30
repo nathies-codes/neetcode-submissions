ab1b864e381c011a5784fce3757dbd1ee6dbadfd
Nathies <149600915+nathies-codes@users.noreply.github.com>
2026-05-30 21:59:26 +0530

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+
+        let freq = {};
+
+        for( let num of nums ) {
+            if( !freq[num] ) {
+                freq[num] = 1;
+            } else {
+                freq[num] ++;
+            }
+        }
+
+        let entries = Object.entries(freq);
+
+        entries.sort((a,b) => b[1] - a[1]);
+
+        let topK = entries.slice(0, k);
+
+        return topK.map(item => Number(item[0]));
+
+    }
+}
