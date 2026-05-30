db8f7170a51b55fa94fe95ffea8d44923a44c135
Nathies <149600915+nathies-codes@users.noreply.github.com>
2026-05-30 22:31:31 +0530

    Add: majority-element - submission-5

---
+++ Data Structures & Algorithms/majority-element/submission-5.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+        let candidate = null;
+        
+        let count = 0
+
+        for ( let num of nums ) {
+            if(count === 0) {
+                candidate = num;
+            } 
+
+            if( num !== candidate) {
+                count --
+            } else {
+                count ++
+            }
+        }
+
+        return candidate;
+    }
+}
