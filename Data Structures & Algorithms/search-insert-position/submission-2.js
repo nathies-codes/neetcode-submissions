f1777cc3f19ab20a4c6665b3664f3183cd4e4ca2
Nathies <149600915+nathies-codes@users.noreply.github.com>
2026-06-03 13:49:10 +0530

    Add: search-insert-position - submission-2

---
+++ Data Structures & Algorithms/search-insert-position/submission-2.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    searchInsert(nums, target) {
+        let left = 0, right = nums.length - 1;
+
+        while ( left <= right) {
+
+            let mid = Math.floor((left + right) / 2);
+
+            if( nums[mid] === target ) return mid;
+
+            if( nums[mid] > target ) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
