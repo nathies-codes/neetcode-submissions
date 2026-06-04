7627a08697525aab38037b3f6271c6b2f0f678c4
Nathies <149600915+nathies-codes@users.noreply.github.com>
2026-06-04 18:14:29 +0530

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    validPalindrome(s) {
+        let left = 0, right = s.length - 1;
+
+        while (left < right) {
+
+            if(s[left] !== s[right]) {
+                return (
+                    isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1)
+                )
+            } else {
+                left ++;
+                right --
+            }
+        }
+
+        return true;
+    }
+}
+
+function isPalindrome( s, left, right ) {
+    while(left < right) {
+        if(s[left] !== s[right]) {
+            return false
+        } else {
+            left ++;
+            right --
+        }
+    }
+    return true;
+}  
