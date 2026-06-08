dddd7394d6edd52000eae682d35f373a4fcd596a
Nathies <149600915+nathies-codes@users.noreply.github.com>
2026-06-08 13:52:12 +0530

    Add: is-palindrome - submission-14

---
+++ Data Structures & Algorithms/is-palindrome/submission-14.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let left = 0,
+            right = s.length - 1;
+
+        while (left < right) {
+            while (left < right && !isAlphaNums(s[left])) {
+                left ++;
+            }
+
+            while (left < right && !isAlphaNums(s[right])) {
+                right --;
+            }
+
+            if(s[left].toLowerCase() !== s[right].toLowerCase()) {
+                return false
+            } else {
+                left ++;
+                right --;
+            }
+        }
+        return true;
+    }
+}
+
+function isAlphaNums(char) {
+    return (
+        (char >= "a" && char <= "z") || (char >= "A" && char <= "Z") || (char >= "0" && char <= "9")
+    );
+}
