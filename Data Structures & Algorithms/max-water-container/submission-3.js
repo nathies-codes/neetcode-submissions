2c8d31ee913a6960c72fdc523eecc2037a76119d
Nathies <149600915+nathies-codes@users.noreply.github.com>
2026-06-12 20:37:40 +0530

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let maxArea = 0;
+
+        let left = 0,
+            right = heights.length - 1;
+
+        while (left < right) {
+            const area = (right - left) * Math.min(heights[left], heights[right]);
+
+            maxArea = Math.max(maxArea, area);
+
+            if (heights[left] < heights[right]) {
+                left ++;
+            } else {
+                right --;
+            }
+        }
+
+        return maxArea;
+    }
+}
