166f813c9ce7a243ca24484668c32d599625877f
Nathies <149600915+nathies-codes@users.noreply.github.com>
2026-06-27 21:52:52 +0530

    Add: trapping-rain-water - submission-5

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-5.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let res = 0;
+
+        if (!height || height.length === 0) return res;
+
+        let left = 0;
+        let right = height.length - 1;
+
+        let leftMax = height[left];
+        let rightMax = height[right];
+
+        while (left < right) {
+            if (leftMax < rightMax) {
+                left ++;
+
+                leftMax = Math.max(leftMax, height[left]);
+
+                res = res + leftMax - height[left];
+            } else {
+                right --;
+
+                rightMax = Math.max(rightMax, height[right]);
+
+                res = res + rightMax - height[right];
+            }
+        }
+
+        return res; 
+    }
+}
