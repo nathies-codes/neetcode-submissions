3d2a936535c2e938d9bc1b18d6b1d651a171e709
Nathies <149600915+nathies-codes@users.noreply.github.com>
2026-07-22 20:54:03 +0530

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let l = 0;
+        let r = nums.length - 1;
+
+        while ( l <= r ) {
+            let mid = Math.floor((l+r)/2);
+
+            if(nums[mid] === target) return mid;
+
+            if(nums[mid] > target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
