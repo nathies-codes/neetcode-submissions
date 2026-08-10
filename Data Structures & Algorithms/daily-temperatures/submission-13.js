39cb6ba0224f0297ca05b6e3cf971d717a28901c
Nathies <149600915+nathies-codes@users.noreply.github.com>
2026-08-10 13:12:49 +0530

    Add: daily-temperatures - submission-13

---
+++ Data Structures & Algorithms/daily-temperatures/submission-13.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+
+        const res = new Array(temperatures.length).fill(0);
+
+        let stack = [];
+
+        for (let i = 0; i < temperatures.length; i ++) {
+
+            let t = temperatures[i];
+
+            while (stack.length > 0 && t > stack[stack.length - 1][0]) {
+
+                let popped = stack.pop();
+
+                let startIndex = popped[1];
+
+                res[startIndex] = i - startIndex;
+            }
+
+            stack.push([t, i])
+        }
+
+        return res;
+    }
+}
