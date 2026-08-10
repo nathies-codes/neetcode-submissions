8aa246601c06a95ef58218d154df476fcd8810db
Nathies <149600915+nathies-codes@users.noreply.github.com>
2026-08-10 17:28:41 +0530

    Add: buy-and-sell-crypto - submission-21

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-21.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let maxProfit = 0;
+
+        let left = 0, right = left + 1;
+
+        for (let price of prices) {
+
+            if (prices[right] > prices[left]) {
+                let profit = prices[right] - prices[left];
+
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                left = right
+            }
+
+            right ++
+
+        }
+
+        return maxProfit;
+    }
+}
