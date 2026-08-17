446944070a22294bd21c2460351fa627536f0936
Nathies <149600915+nathies-codes@users.noreply.github.com>
2026-08-17 18:21:40 +0530

    Add: two-integer-sum-ii - submission-14

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-14.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+
+        let left = 0;
+        let right = numbers.length - 1;
+
+        while (right < numbers.length) {
+
+            let twoSum = numbers[left] + numbers[right];
+
+            if (twoSum > target) {
+                right --;
+            } else if (twoSum < target) {
+                left ++;
+            } else {
+                return (
+                    [left + 1, right + 1]
+                )
+            }
+        }
+
+        return ([]);
+
+    }
+}
